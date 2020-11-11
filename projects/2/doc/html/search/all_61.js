27e3c64f360deade3e3423ab5b1aa5b0752b817a
wonsunahn <wonsunahn@gmail.com>
2020-11-11 17:42:03 -0500

    Formatting changes with CacheLine

--- projects/2/doc/html/search/all_61.js
+++ projects/2/doc/html/search/all_61.js
@@ -4,6 +4,7 @@ var searchData=
   ['accessline',['accessLine',['../classCacheCore.html#a1b04af86349aaa9a3256397816ab42f1',1,'CacheCore']]],
   ['addlatency',['addLatency',['../classMemRequest.html#a41e03aafdc8c06a1e38986e92dc2681f',1,'MemRequest']]],
   ['addr',['addr',['../classMemRequest.html#aabbd26b9f5ad9c6eafb1cf4ebf105b8e',1,'MemRequest']]],
+  ['age',['age',['../classCacheLine.html#abe57bb750b7e997c768524efb91f29e8',1,'CacheLine']]],
   ['allocateline',['allocateLine',['../classCacheCore.html#a5477912314038fe80f0452f62b5b47aa',1,'CacheCore']]],
   ['assoc',['assoc',['../classCacheCore.html#a741a89b3997a411730acf043cf7a2ee3',1,'CacheCore']]]
 ];
--- projects/2/doc/html/search/all_74.js
+++ projects/2/doc/html/search/all_74.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['tostring',['toString',['../classCache.html#ac0f8f75bcf4db0a5f90be4f4f4e7ffaa',1,'Cache::toString()'],['../classCacheCore.html#acc4c03b4da2c9bacb1ee39c9a15abdfb',1,'CacheCore::toString()'],['../classDRAM.html#a4ec0033bd1f70655e9fdf80a38e77db3',1,'DRAM::toString()'],['../classMemObj.html#a014a408e2555dfc398dba42ccac7508b',1,'MemObj::toString()']]]
+  ['tag',['tag',['../classCacheLine.html#a901e846810707438e7d13b317ea4ffaf',1,'CacheLine']]],
+  ['tostring',['toString',['../classCache.html#ac0f8f75bcf4db0a5f90be4f4f4e7ffaa',1,'Cache::toString()'],['../classCacheCore.html#acc4c03b4da2c9bacb1ee39c9a15abdfb',1,'CacheCore::toString()'],['../classCacheLine.html#ac1351e3d4e45e9d497579287addde52a',1,'CacheLine::toString()'],['../classDRAM.html#a4ec0033bd1f70655e9fdf80a38e77db3',1,'DRAM::toString()'],['../classMemObj.html#a014a408e2555dfc398dba42ccac7508b',1,'MemObj::toString()']]]
 ];
